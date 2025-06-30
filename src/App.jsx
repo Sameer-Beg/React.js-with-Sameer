57cdeea8e6cfe3774b0445747e4d2b234e3e3cf5
SameerBeg <sameer423j@gmail.com>
2025-06-30 12:46:46 +0530

    Add files

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import V1_variabledeclare from './Components/V1_variabledeclare'
+import V2_reusableComponent from './Components/V2_reusableComponent'
+
+
 const App = () => {
   return (
     <>
-    <div>
+    {/* <div>
       <V1_variabledeclare/>
+    </div> */}
+
+    <div>
+      <V2_reusableComponent/>
+      <V2_reusableComponent/>
+      <V2_reusableComponent/>
+      <V2_reusableComponent/>
     </div>
     </>
   )
---
+++ src/Components/V2_reusableComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const V2_reusableComponent = () => {
+  return (
+    <>
+    <div>
+        <h1>Sameer is a good full stack developer</h1>
+    </div>
+    </>
+  )
+}
+
+export default V2_reusableComponent
