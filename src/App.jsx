867db32713af8a1be5c5030f7782f565c089bf57
SameerBeg <sameer423j@gmail.com>
2025-06-30 12:42:04 +0530

    Add files and code

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import V1_variabledeclare from './Components/V1_variabledeclare'
+const App = () => {
+  return (
+    <>
+    <div>
+      <V1_variabledeclare/>
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/V1_variabledeclare.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const V1_variabledeclare = () => {
+    const name ="sameer beg"
+    const age = 20
+  return (
+    <>
+    <div>
+        <h1>{name}</h1>
+        <h2>{age}</h2>
+    </div>
+    </>
+  )
+}
+
+export default V1_variabledeclare
